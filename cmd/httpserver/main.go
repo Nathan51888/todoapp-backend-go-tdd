3463a4032fa929bbe742c08c2c5ee87bcddcd1af
Nathan51888 <NathanLiang99@outlook.com>
2025-03-01 16:54:21 +1300

    added postgresql todo store to main application

--- cmd/httpserver/main.go
+++ cmd/httpserver/main.go
@@ -3,15 +3,12 @@ package main
 import (
 	"log"
 	"mytodoapp/adapters/httpserver"
-	"mytodoapp/adapters/persistence/inmemory"
-	"mytodoapp/domain/todo"
+	"mytodoapp/adapters/persistence/postgre"
 	"net/http"
 )
 
 func main() {
-	store := &inmemory.InMemoryTodoStore{Todos: []todo.Todo{
-		{Title: "Todo1", Completed: "false"},
-	}}
+	store := postgre.NewPostgreTodoStore()
 	server := httpserver.NewTodoServer(store)
 	log.Fatal(http.ListenAndServe(":8080", server))
 }
