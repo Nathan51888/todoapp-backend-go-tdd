8a71b3da0e9228f322ae1b15cc7fd9acd5abfb0a
Nathan51888 <NathanLiang99@outlook.com>
2025-03-15 18:02:24 +1300

    changed todo id type to uuid and added user table

--- testing/testdata/init_db.sql
+++ testing/testdata/init_db.sql
@@ -5,12 +5,27 @@
 -- DROP TABLE public.todo;
 
 CREATE TABLE public.todo (
-    id serial4 NOT NULL,
+    id uuid DEFAULT gen_random_uuid() NOT NULL,
     title varchar NULL,
-    completed boolean,
-    CONSTRAINT todo_pk PRIMARY KEY (id)
+    completed bool NULL,
+    CONSTRAINT todo_pk PRIMARY KEY (id),
+    CONSTRAINT todo_user_fk FOREIGN KEY (id) REFERENCES public."user" (id)
 );
 
 -- INSERT INTO public.todo
--- (title, completed, id)
--- VALUES ('Test_todo', 'false', nextval('todo_id_seq'::regclass));
+-- (title, completed)
+-- VALUES ('Test_todo', 'false');
+
+
+-- public."user" definition
+
+-- Drop table
+
+-- DROP TABLE public."user";
+
+CREATE TABLE public."user" (
+    id uuid NOT NULL,
+    email varchar NOT NULL,
+    "password" varchar NOT NULL,
+    CONSTRAINT user_pk PRIMARY KEY (id)
+);
