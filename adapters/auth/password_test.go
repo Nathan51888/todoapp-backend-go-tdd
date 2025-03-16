7862ba8c2cbfe313e002f32eb312178d329a4131
Nathan51888 <NathanLiang99@outlook.com>
2025-03-16 18:22:00 +1300

    updated password tests

--- adapters/auth/password_test.go
+++ adapters/auth/password_test.go
@@ -9,8 +9,26 @@ import (
 
 func TestHashPassword(t *testing.T) {
 	password := "password"
-	hashedPassword, err := auth.HashPassword(password)
+	hash, err := auth.HashPassword(password)
 	assert.NoError(t, err, "HashPassword()")
-	isCorrect := auth.ComparePassword(hashedPassword, password)
-	assert.Equal(t, true, isCorrect)
+
+	if hash == "" {
+		t.Error("expected hash to be not empty")
+	}
+	if hash == password {
+		t.Error("expected hash to be different from original text")
+	}
+}
+
+func TestComparePassword(t *testing.T) {
+	password := "password"
+	hash, err := auth.HashPassword(password)
+	assert.NoError(t, err, "HashPassword()")
+
+	if !auth.ComparePassword(hash, password) {
+		t.Error("expected password to match hash")
+	}
+	if auth.ComparePassword(hash, "not password") {
+		t.Error("expected password to not match hash")
+	}
 }
