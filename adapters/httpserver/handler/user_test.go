9eecce97e68ed91253e095d39a94248f7cdf70c6
Nathan51888 <NathanLiang99@outlook.com>
2025-03-24 14:57:38 +1300

    added test user profile for user handler

--- adapters/httpserver/handler/user_test.go
+++ adapters/httpserver/handler/user_test.go
@@ -11,6 +11,7 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -109,39 +110,39 @@ func TestUserLoginHandler(t *testing.T) {
 	})
 }
 
-// func TestUserProfile(t *testing.T) {
-// 	t.Run("can get user profile with valid token", func(t *testing.T) {
-// 		mux := http.NewServeMux()
-// 		password, err := auth.HashPassword("password")
-// 		if err != nil {
-// 			t.Fatal(err)
-// 		}
-// 		userId := uuid.New()
-// 		store := &inmemory.InMemoryUserStore{Users: []user.User{
-// 			{Id: userId, Email: "user@email.com", Password: password},
-// 		}}
-// 		handler.NewUserHandler(mux, store)
-//
-// 		token, err := auth.CreateJWT([]byte(auth.JWTSecret), userId.String())
-// 		if err != nil {
-// 			t.Fatal(err)
-// 		}
-//
-// 		req := httptest.NewRequest(http.MethodPost, "/profile", nil)
-// 		req.Header.Add("Authorization", token)
-// 		res := httptest.NewRecorder()
-//
-// 		mux.ServeHTTP(res, req)
-//
-// 		want := handler.UserProfilePayload{
-// 			Email:     "user@email.com",
-// 			FirstName: "first",
-// 			LastName:  "last",
-// 			Birthday:  "birthday",
-// 		}
-// 		var got handler.UserProfilePayload
-// 		json.NewDecoder(res.Body).Decode(&got)
-// 		assert.Equal(t, want, got)
-// 		assert.Equal(t, http.StatusOK, res.Code)
-// 	})
-// }
+func TestUserProfile(t *testing.T) {
+	t.Run("can get user profile with valid token", func(t *testing.T) {
+		mux := http.NewServeMux()
+		password, err := auth.HashPassword("password")
+		if err != nil {
+			t.Fatal(err)
+		}
+		userId := uuid.New()
+		store := &inmemory.InMemoryUserStore{Users: []user.User{
+			{Id: userId, Email: "user@email.com", Password: password},
+		}}
+		handler.NewUserHandler(mux, store)
+
+		token, err := auth.CreateAccessToken(userId.String())
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		req := httptest.NewRequest(http.MethodGet, "/profile", nil)
+		req.Header.Add("Authorization", token)
+		res := httptest.NewRecorder()
+
+		mux.ServeHTTP(res, req)
+
+		want := handler.UserProfilePayload{
+			Email:     "user@email.com",
+			FirstName: "first",
+			LastName:  "last",
+			Birthday:  "birthday",
+		}
+		var got handler.UserProfilePayload
+		json.NewDecoder(res.Body).Decode(&got)
+		assert.Equal(t, want, got)
+		assert.Equal(t, http.StatusOK, res.Code)
+	})
+}
